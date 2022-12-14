5b5716a9a99dca94ee3a1e16e4f6f4b5d08dff30
Felipe Alzate <jfelipe.alzate@gmail.com>
2022-12-14 16:47:23 -0500

    [add] gsap animation for experience section

---
+++ js/experiences.js
@@ -0,0 +1,26 @@
+
+// const experiences_left = document.querySelectorAll('.experience_left');
+// const experiences_right = document.querySelectorAll('.experience_right');
+
+// experiences_left.forEach(experience => {
+//     gsap.fromTo(experience, {duration: 2, x: -100, ease: 'expo', 
+//         scrollTrigger: {
+//             trigger: experience,
+//             start: 'top 75%',
+//             end: 'bottom 25%',
+//             toggleActions: 'restart reset restart'
+//         } 
+//     })
+// });
+
+// experiences_right.forEach(experience => {
+//     gsap.fromTo(experience, {duration: 2, x: 100, ease: 'expot', 
+//         scrollTrigger: {
+//             trigger: experience,
+//             start: 'top 75%',
+//             end: 'bottom 25%',
+//             toggleActions: 'restart reset restart'
+//         } 
+//     })
+// });
+
